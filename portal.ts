d10410ae844bc1cc5bbe961d618e09b2775b559f
pooja15-2007 <n.poojamaheswari@gmail.com>
2026-04-02 22:34:35 +0530

    Create portal.ts

---
+++ portal.ts
@@ -0,0 +1,113 @@
+interface Job{
+id:number
+title:string
+company:string
+location:string
+salary:string
+role:string
+type:string
+}
+
+const jobs:Job[]=[
+{
+id:1,
+title:"Frontend Developer",
+company:"Google",
+location:"Bangalore",
+salary:"₹12 LPA",
+role:"Frontend",
+type:"Fulltime"
+},
+{
+id:2,
+title:"Backend Engineer",
+company:"Amazon",
+location:"Hyderabad",
+salary:"₹15 LPA",
+role:"Backend",
+type:"Remote"
+},
+{
+id:3,
+title:"Fullstack Developer",
+company:"Microsoft",
+location:"Chennai",
+salary:"₹18 LPA",
+role:"Fullstack",
+type:"Hybrid"
+},
+{
+id:4,
+title:"Frontend Engineer",
+company:"Infosys",
+location:"Remote",
+salary:"₹7 LPA",
+role:"Frontend",
+type:"Remote"
+}
+]
+
+const jobList=document.getElementById("jobList") as HTMLElement
+const searchInput=document.getElementById("searchInput") as HTMLInputElement
+const roleFilter=document.getElementById("roleFilter") as HTMLSelectElement
+const locationFilter=document.getElementById("locationFilter") as HTMLSelectElement
+
+function renderJobs(jobArray:Job[]){
+
+jobList.innerHTML=""
+
+jobArray.forEach(job=>{
+
+const div=document.createElement("div")
+div.className="job"
+
+div.innerHTML=`
+
+<h3>${job.title}</h3>
+<p><b>${job.company}</b></p>
+<p>${job.location}</p>
+<p>${job.salary}</p>
+
+<div class="tags">
+<span class="tag">${job.role}</span>
+<span class="tag">${job.type}</span>
+</div>
+
+<button class="apply">Apply Now</button>
+
+`
+
+jobList.appendChild(div)
+
+})
+
+}
+
+function filterJobs(){
+
+const search=searchInput.value.toLowerCase()
+const role=roleFilter.value
+const location=locationFilter.value
+
+const filtered=jobs.filter(job=>{
+
+return(
+(job.title.toLowerCase().includes(search) ||
+job.company.toLowerCase().includes(search)) &&
+
+(role==="all" || job.role===role) &&
+(location==="all" || job.location===location)
+
+)
+
+})
+
+renderJobs(filtered)
+
+}
+
+searchInput.addEventListener("input",filterJobs)
+roleFilter.addEventListener("change",filterJobs)
+locationFilter.addEventListener("change",filterJobs)
+
+renderJobs(jobs)
